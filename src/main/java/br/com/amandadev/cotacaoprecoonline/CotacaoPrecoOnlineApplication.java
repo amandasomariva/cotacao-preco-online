177ea72fe35448948e89c6edd7ceaf3bc62350ac
Amanda Somariva <amanda_somariva@hotmail.com>
2020-05-16 15:55:09 -0300

    Inicio do projeto cotação de preços online

---
+++ src/main/java/br/com/amandadev/cotacaoprecoonline/CotacaoPrecoOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.amandadev.cotacaoprecoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotacaoPrecoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotacaoPrecoOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amandadev/cotacaoprecoonline/CotacaoPrecoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amandadev.cotacaoprecoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacaoPrecoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
