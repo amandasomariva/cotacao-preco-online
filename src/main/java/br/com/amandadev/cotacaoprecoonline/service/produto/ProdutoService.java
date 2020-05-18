42e823493734e179d42a0090a6ef36b38d0710f1
Amanda Somariva <amanda_somariva@hotmail.com>
2020-05-17 21:40:38 -0300

    Criado serviço para buscar por cotações pelo id do produto e removido a responsabilidade pelo retorno da resposta ao controller

--- src/main/java/br/com/amandadev/cotacaoprecoonline/service/produto/ProdutoService.java
+++ src/main/java/br/com/amandadev/cotacaoprecoonline/service/produto/ProdutoService.java
@@ -1,6 +1,7 @@
 package br.com.amandadev.cotacaoprecoonline.service.produto;
 
 import br.com.amandadev.cotacaoprecoonline.error.ResourceNotFoundException;
+import br.com.amandadev.cotacaoprecoonline.model.cotacao.Cotacao;
 import br.com.amandadev.cotacaoprecoonline.model.produto.Produto;
 import br.com.amandadev.cotacaoprecoonline.repository.produto.ProdutoRepository;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,10 +25,11 @@ public class ProdutoService {
         return this.produtoRepository.findAll(pageable);
     }
 
-    public ResponseEntity<Produto> findById(Long id) {
+//    fazer todos assim, pois a responsabilidade de retorno de resposta é no controller
+    public Produto findById(Long id) {
         Optional<Produto> produto = this.produtoRepository.findById(id);
         if(produto.isPresent()){
-            return new ResponseEntity<Produto>(produto.get(), HttpStatus.OK);
+            return produto.get();
         }
         throw new ResourceNotFoundException("Não existe nenhum produto com esse id" + id);
     }
@@ -42,9 +45,14 @@ public class ProdutoService {
     public ResponseEntity<Produto> deleteById(Long id){
         boolean produtoExiste = this.produtoRepository.existsById(id);
         if (produtoExiste) {
-                this.produtoRepository.deleteById(id);
-                return new ResponseEntity<>(HttpStatus.OK);
-            }
-            throw new ResourceNotFoundException("Não existe nenhum produto com esse id" + id + "para ser deletado");
+            this.produtoRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        throw new ResourceNotFoundException("Não existe nenhum produto com esse id" + id + "para ser deletado");
+    }
+
+    public ResponseEntity<List<Cotacao>> findCotacaoByIdProduto(Long id) {
+        List<Cotacao> cotacaoByProdutoId = this.produtoRepository.findCotacaoByProdutoId(id);
+        return new ResponseEntity<>(cotacaoByProdutoId, HttpStatus.OK);
     }
 }
