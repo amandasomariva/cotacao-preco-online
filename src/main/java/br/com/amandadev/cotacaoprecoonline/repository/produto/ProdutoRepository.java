f31cfb3c6559fd71cc104179f082c6b35231b6b3
Amanda Somariva <amanda_somariva@hotmail.com>
2020-05-17 21:38:32 -0300

    Criado Query para buscar cotações de produto pelo id

--- src/main/java/br/com/amandadev/cotacaoprecoonline/repository/produto/ProdutoRepository.java
+++ src/main/java/br/com/amandadev/cotacaoprecoonline/repository/produto/ProdutoRepository.java
@@ -1,8 +1,15 @@
 package br.com.amandadev.cotacaoprecoonline.repository.produto;
 
+import br.com.amandadev.cotacaoprecoonline.model.cotacao.Cotacao;
 import br.com.amandadev.cotacaoprecoonline.model.produto.Produto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Long> {
+
+    @Query("select c from Cotacao c where c.produto.id = ?1")
+    List<Cotacao> findCotacaoByProdutoId(Long produtoId);
 
 }
